8ec8f1bb6a1d5b256998ebf1b754a9fddeb86b24
tla111 <tlamarketing111@gmail.com>
2019-01-02 15:20:15 -0500

    made changes

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("button").addEventListener("click", () => 
+{
+    document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".bg-modal").style.display = "none";
+});
